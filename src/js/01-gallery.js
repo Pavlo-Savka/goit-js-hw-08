267feb7362dd757e5720eb092ee9be8bea7db4c1
Pavlo-Savka <savka.pavlo@gmail.com>
2023-02-21 00:07:07 +0200

    Task 1, 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,44 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+const basicLightbox = require("simplelightbox");
 const gallery = document.querySelector(".gallery");
+
 for (let item of galleryItems) {
     let largeImg = item.original; 
     let smallImg = item.preview;
     let description = item.description;
 gallery.insertAdjacentHTML('beforeend', `<div class= "gallery__item">
-  <a class="gallery__link" href="${largeImg}">
+  <a href="${largeImg}">
     <img
-      class="gallery__image"
       src="${smallImg}"
-      data-source="large-image.jpg"
       alt="${description}"
     />
   </a>
 </div>`)
 };
 
-const selectImg = (event) => {
-  if (event.target.nodeName !== "IMG")
-   return;
-  const selectedImg = event.target.parentNode.href;
-  event.preventDefault();
-  const instance = basicLightbox.create(`<img src="${selectedImg}" width = "800" height = "600">`,
-    {
-      onShow: () =>
-        document.addEventListener("keydown", handleKeyDown),
-      onClose: () =>
-        document.removeEventListener("keydown", handleKeyDown),
-    }
-  );
-  instance.show();
-  function handleKeyDown(event) {
-    if (event.code === 'Escape') {
-      instance.close();
-    };
-  };  
-};
-gallery.addEventListener("click", selectImg);
+new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250,
+ });
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+//import throttle from 'lodash.throttle';
+  const iframe = document.querySelector('iframe');
+  const player = new Vimeo.Player(iframe);
+  const throttle = require('lodash.throttle');
+  
+player.on('timeupdate', throttle(currentTimePlay, 1000));
+function currentTimePlay (time) {
+    localStorage.setItem("videoplayer-current-time", time.seconds);
+};
+ player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function() {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
 
+        default:
+            // some other error occurred
+            break;
+    }
+});
