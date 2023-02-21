ca96e76ab706d3053c4fc1a5f67eef054229f33c
Pavlo-Savka <savka.pavlo@gmail.com>
2023-02-22 01:33:46 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+ const throttle = require('lodash.throttle');
+ const form = document.querySelector(".feedback-form");
+function handleSubmit(event) {
+    event.preventDefault();
+};
+form.addEventListener("input", throttle(handleInput, 500));
+form.addEventListener("submit", handleSubmit);
+
+function handleInput() { 
+    localStorage.setItem("feedback-form-state", 
+    JSON.stringify({email: form.email.value, message: form.message.value}));
+};
+
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log(localStorage.getItem("feedback-form-state"));
+    localStorage.clear("feedback-form-state");
+    form.reset();
+}
+
+try {
+   form.email.value = JSON.parse(localStorage.getItem("feedback-form-state")).email;
+}
+catch (e) {
+    form.email.value = ""
+};
+
+try {
+   form.message.value = JSON.parse(localStorage.getItem("feedback-form-state")).message;
+}
+catch (e) {
+    form.message.value = ""
+};
 
